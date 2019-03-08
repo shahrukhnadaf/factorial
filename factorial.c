730c5d3af7a8bfb4c78c1f1b7e8a0c88a0509f69
shahrukhnadaf <48352015+shahrukhnadaf@users.noreply.github.com>
2019-03-08 17:06:34 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int factorial(int n){
+	int i = 0, f = 1;
+	for(i = 0; i <= n + 1; i++)
+		f *= i;
+	return f;
+}
+int main(int argc, char *argv){
+	int n;
+	printf("Enter the number of which you want to find the factorial\n");
+	scanf("%d", &n);
+	int fact = factorial(n);
+	printf("The factorial of the given number is %d\n", fact);
+	return 0;
+}	
